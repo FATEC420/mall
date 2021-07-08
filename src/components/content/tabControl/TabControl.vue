ef1790768566f7283200acba593806ce31f6260c
FATEC420 <2041481502@qq.com>
2021-07-08 20:12:31 +0800

    7.8加

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="tab-control">
+    <div
+      class="tab-control-item"
+      v-for="(it, index) in titles"
+      :key="index"
+      :class="{ active: index === currentIndex }"
+      @click="itClick(index)"
+    >
+      <span>{{ it }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabControl",
+  props: {
+    titles: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  data() {
+    return {
+      currentIndex: 0,
+    };
+  },
+  methods: {
+    itClick(index) {
+      this.currentIndex = index;
+      this.$emit("tabClick", index);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.tab-control {
+  display: flex;
+  width: 100%;
+  text-align: center;
+  vertical-align: middle;
+  height: 40px;
+  font-size: 15px;
+  background-color: #fff;
+}
+.tab-control-item {
+  flex: 1;
+  line-height: 40px;
+}
+.tab-control-item span {
+  padding: 5px;
+}
+.active {
+  color: hotpink;
+}
+.active span {
+  border-bottom: 3px solid hotpink;
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,14 +3,27 @@
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
     <home-swiper :banners="banners"></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control
+      class="tab-control"
+      :titles="['流行', '新款', '精选']"
+      @tabClick="tabClick"
+    ></tab-control>
+    <goods-list :goods="showGoods"></goods-list>
   </div>
 </template>
 
 <script>
+//公共组件
 import NavBar from "components/common/navbar/NavBar";
-import { getHomeMultidata } from "network/home";
+import TabControl from "components/content/tabControl/TabControl";
+import GoodsList from "components/content/goods/GoodsList";
+//子组件
 import HomeSwiper from "./childComps/HomeSwiper.vue";
-import RecommendView from "./childComps/RecommendView.vue";
+import RecommendView from "./childComps/RecommendView";
+import FeatureView from "./childComps/featureView";
+//方法
+import { getHomeMultidata, getHomeGoods } from "network/home";
 
 export default {
   name: "Home",
@@ -18,26 +31,85 @@ export default {
     NavBar,
     HomeSwiper,
     RecommendView,
+    FeatureView,
+    TabControl,
+    GoodsList,
   },
   data() {
     return {
       banners: [],
       recommends: [],
+      goods: {
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] },
+      },
+      currentType: "pop",
     };
+  },
+  computed: {
+    showGoods() {
+      return this.goods[this.currentType].list;
+    },
   },
   created() {
     //请求多个数据
-    getHomeMultidata().then((res) => {
-      this.banners = res.data.banner.list;
-      this.recommends = res.data.recommend.list;
-    });
+    this.getHomeMultidata(),
+      //请求商品信息
+      this.getHomeGoods("pop");
+    this.getHomeGoods("new"), this.getHomeGoods("sell");
+  },
+  methods: {
+    //事件监听方法
+    tabClick(index) {
+      switch (index) {
+        case 0:
+          this.currentType = "pop";
+          break;
+        case 1:
+          this.currentType = "new";
+          break;
+        case 2:
+          this.currentType = "sell";
+      }
+    },
+    // 网络请求相关方法
+    getHomeMultidata() {
+      getHomeMultidata().then((res) => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      });
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type, page).then((res) => {
+        // console.log(res);
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page += 1; /* 更新data中的页码 */
+        //this.$refs.scroll.finishPullUp(); // 调用方法，多次上拉加载更多
+      });
+    },
   },
 };
 </script>
 
 <style scoped>
+#home {
+  padding-top: 44px;
+}
 .home-nav {
   background-color: pink;
   color: #fff;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
+}
+.tab-control {
+  /* 设置停留 */
+  position: sticky;
+  top: 44px;
+  z-index: 9;
 }
 </style>
